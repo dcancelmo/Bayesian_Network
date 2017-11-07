0e982d0da3d0a14b6de456864da4fb96d1b635fe
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-11-06 23:18:01 -0500

    Enumeration-ask done?

--- src/bn/inference/Enumeration.java
+++ src/bn/inference/Enumeration.java
@@ -6,16 +6,23 @@ import bn.core.Distribution;
 import bn.core.RandomVariable;
 import bn.parser.XMLBIFParser;
 
+import java.util.List;
+
 public class Enumeration implements Inferencer {
 
     @Override
     public Distribution ask(BayesianNetwork bn, RandomVariable X, Assignment e) {
         Distribution q = new Distribution(X);
         for (Object xi : X.getDomain()) {
-             //q.put(all(), xi);
+             q.put(xi, all(bn.getVariableListTopologicallySorted(), e));
         }
         q.normalize();
         return q;
+    }
+
+    public float all(List<RandomVariable> vars, Assignment e) {
+
+        return 0.0f;
     }
 
 }
